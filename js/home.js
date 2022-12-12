10907881469a785358da52127360c2e7477f8cad
pr3khar <prakhar19376@iiitd.ac.in>
2022-12-13 02:07:26 +0530

    animations + my story

--- js/home.js
+++ js/home.js
@@ -1,61 +1,19 @@
-var $tickerWrapper = ".tickerwrapper";
-var $list = $tickerWrapper.find("ul.list");
-var $clonedList = $list.clone();
-var listWidth = 10;
-
-$list.find("li").each(function (i) {
-  listWidth += $(this, i).outerWidth(true);
+new Vivus("animation-1", {
+  type: "delayed",
+  duration: 200,
+  animTimingFunction: Vivus.EASE,
 });
 
-var endPos = $tickerWrapper.width() - listWidth;
-
-$list.add($clonedList).css({
-  width: listWidth + "px",
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayTimeout: 16000,
+  autoplayHoverPause: true,
+  dots: true,
+  dotsEach: true,
+  nav: true,
+  navText: true,
+  center: true,
+  margin: 30,
 });
-
-$clonedList.addClass("cloned").appendTo($tickerWrapper);
-
-//TimelineMax
-var infinite = new TimelineMax({ repeat: -1, paused: true });
-var time = 40;
-
-infinite
-  .fromTo(
-    $list,
-    time,
-    { rotation: 0.01, x: 0 },
-    { force3D: true, x: -listWidth, ease: Linear.easeNone },
-    0
-  )
-  .fromTo(
-    $clonedList,
-    time,
-    { rotation: 0.01, x: listWidth },
-    { force3D: true, x: 0, ease: Linear.easeNone },
-    0
-  )
-  .set($list, { force3D: true, rotation: 0.01, x: listWidth })
-  .to(
-    $clonedList,
-    time,
-    { force3D: true, rotation: 0.01, x: -listWidth, ease: Linear.easeNone },
-    time
-  )
-  .to(
-    $list,
-    time,
-    { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone },
-    time
-  )
-  .progress(1)
-  .progress(0)
-  .play();
-
-//Pause/Play
-$tickerWrapper
-  .on("mouseenter", function () {
-    infinite.pause();
-  })
-  .on("mouseleave", function () {
-    infinite.play();
-  });
