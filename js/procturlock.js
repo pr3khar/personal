ef35d0fe7a9b9a09e393191617e1c3c4383251a2
pr3khar <prakhar19376@iiitd.ac.in>
2022-12-15 00:27:00 +0530

    now page & bug fixes

--- js/procturlock.js
+++ js/procturlock.js
@@ -1,10 +1,10 @@
-function checkPswd() {
+https: function checkPswd() {
   var confirmPassword = "prakharproctur";
   //since you are here, I guess you have enough time at your disposal. Please develop a better system for me. Have a good day :)
   var password = document.getElementById("pswd").value;
   if (password == confirmPassword) {
     window.location =
-      "https://pr3khar.notion.site/Internship-at-Upstox-07d6895a70854306a744c2b1abf5435d";
+      "//pr3khar.notion.site/Proctur-57bdfb9e99d3461bb5d1fc9522927f59";
   } else {
     alert("Incorrect :(");
   }
